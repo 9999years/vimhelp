3cfbe7ed68883915c7bbf5863983ed75f82b4937
Rebecca Turner <rbt@sent.as>
2022-06-05 17:24:25 -0400

    Spruce up script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Cloning https://github.com/vim/vim.git"
 tmp=$(mktemp -d)
-pushd "$tmp"
+pushd "$tmp" >/dev/null || exit
 git clone --depth 1 --sparse https://github.com/vim/vim.git
-cd vim
+cd vim || exit
 git sparse-checkout set runtime/doc
-popd
+popd >/dev/null || exit
 
 vim_dir="$tmp/vim/runtime/doc"
 
+echo "Generating helptags with Vim"
 vim "+helptags $vim_dir" "+helptags $INPUT_DOC_DIRECTORY" +q
 
+echo "Generating HTML documentation"
 python3 "$(dirname "$0")/scripts/h2h.py" "$INPUT_DOC_DIRECTORY" "$vim_dir" "$INPUT_OUTPUT_DIRECTORY"
